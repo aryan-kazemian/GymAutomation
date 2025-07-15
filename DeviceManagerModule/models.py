b40612e39c1b512d30ef54e8b04474cdb73b6291
Aryan <kazemianaryan@gmail.com>
2025-07-15 18:09:03 +0330

    Fix Update

--- DeviceManagerModule/models.py
+++ DeviceManagerModule/models.py
@@ -2,17 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-
 class Device(models.Model):
-    deviceId = models.CharField(max_length=100)
-    deviceType = models.CharField(max_length=200)
-    deviceGroup = models.CharField(max_length=100)
-    deviceName = models.CharField(max_length=100)
-    devicePort = models.CharField(max_length=100)
-    deviceIP = models.CharField(max_length=100)
-    baudRate = models.CharField(max_length=100)
-    priority = models.CharField(max_length=100)
-    dataBits = models.CharField(max_length=100)
-    stopBits = models.CharField(max_length=100)
-    timeout = models.CharField(max_length=100)
-    protocol = models.CharField(max_length=100)
+    deviceId = models.CharField(max_length=100, blank=True, null=True)
+    deviceType = models.CharField(max_length=200, blank=True, null=True)
+    deviceGroup = models.CharField(max_length=100, blank=True, null=True)
+    deviceName = models.CharField(max_length=100, blank=True, null=True)
+    devicePort = models.CharField(max_length=100, blank=True, null=True)
+    deviceIP = models.CharField(max_length=100, blank=True, null=True)
+    baudRate = models.CharField(max_length=100, blank=True, null=True)
+    priority = models.CharField(max_length=100, blank=True, null=True)
+    dataBits = models.CharField(max_length=100, blank=True, null=True)
+    stopBits = models.CharField(max_length=100, blank=True, null=True)
+    timeout = models.CharField(max_length=100, blank=True, null=True)
+    protocol = models.CharField(max_length=100, blank=True, null=True)
