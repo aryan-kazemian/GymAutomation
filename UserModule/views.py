61d7df020f79e9f3b379302066310703f8bfa3f9
aryan-kazemian <kazemianaryan@gmail.com>
2025-07-29 13:10:05 +0330

    FIxUpdate

--- UserModule/views.py
+++ UserModule/views.py
@@ -93,7 +93,6 @@ class DynamicAPIView(APIView):
                     'shift_description': member.shift.shift_desc if member.shift else None,
                     'creation_datetime': member.creation_datetime,
                     'full_name': member.person.full_name if member.person else None,
-                    'price': member.price,
                     'card_no': member.card_no,
                     'membership_type': member.membership_type,
                     'membership_datetime': member.membership_datetime,
