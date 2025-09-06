4a4fac24ab50868a73892aaceafea4d1d2594f50
aryan-kazemian <kazemianaryan@gmail.com>
2025-09-06 17:53:30 +0330

    Update .gitignore and remove build artifacts

--- dist_obf/pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 9.1.8 (trial), 000000, 2025-09-04T12:26:43.174183
-from .pyarmor_runtime import __pyarmor__
--- license_utils.py
+++
@@ -1,8 +0,0 @@
-import uuid
-
-def get_machine_id():
-    """
-    Returns a unique identifier for this machine.
-    Uses the MAC address of the first network interface.
-    """
-    return str(uuid.getnode())
